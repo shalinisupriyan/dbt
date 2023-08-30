f848d4c5cf8e17a78f6693a4c2ab8422492cd448
shalinisupriyan@gmail.com <shalinisupriyan@gmail.com>
2023-08-30 18:58:42 +0000

    commit

--- models/production_backlog.sql
+++
@@ -1,6 +0,0 @@
--- Model: production_backlog.sql
-select
-    month,
-    units_planned - units_manufactured as production_backlog
-from
-    {{ ref('manufactured_planned') }};
--- models/production_plan.sql
+++ models/production_plan.sql
@@ -1,8 +1,25 @@
+-- models/production_plan.sql
+
 {{ config(
-    materialized='view'
+    materialized='table',
+    unique_key='month'
 ) }}
+
+with production_data as (
+    select
+        date_trunc('month', date) as month,
+        pn,
+        sum(plan) as planned_units
+    from
+        {{ source('elephantsql_source', 'production_plan') }}
+    group by
+        1, 2
+)
 
 select
     *,
-    date_trunc('month', date) as month
-from {{ source('my_source', 'production_plan') }}
+    lag(planned_units, 1, 0) over (order by month) as cumulative_planned_units
+from
+    production_data
+order by
+    month;
